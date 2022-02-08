7b86320814f30a22904ddebfb7d9b42a4baa05c4
DANIEL BULMAN <D.BULMAN-2020@hull.ac.uk>
2022-02-08 15:44:13 +0000

    Allowed client to query test server

--- location/location.cs
+++ location/location.cs
@@ -8,20 +8,30 @@ namespace location
     {
         static void Main(string[] args)
         {
-            if (args.Length != 0)
+            if (args.Length != 0 && args.Length<=2)
             {
-                int c;
                 TcpClient client = new TcpClient();
                 client.Connect("whois.net.dcs.hull.ac.uk", 43);
                 StreamWriter sw = new StreamWriter(client.GetStream());
                 StreamReader sr = new StreamReader(client.GetStream());
-                sw.WriteLine(args[0]);
+                if (args.Length == 1)
+                {
+                    sw.WriteLine(args[0]);
+                }
+                else if (args.Length == 2) 
+                {
+                    sw.WriteLine(args[0] + " " + args[1]);
+                }
                 sw.Flush();
                 Console.WriteLine(sr.ReadToEnd());
             }
-            else
+            else if (args.Length==0)
             {
                 Console.WriteLine("No arguments where supplied!");
+            }
+            else 
+            {
+                Console.WriteLine("Too many arguments where supplied!");
             }
         }
     }
