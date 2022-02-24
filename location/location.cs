ca44f7c123719e8c0f5cdfcb5af3b5fe80d9fb44
DANIEL BULMAN <D.BULMAN-2020@hull.ac.uk>
2022-02-24 18:58:43 +0000

    Client completes all tests for Lab 3 as required

--- location/location.cs
+++ location/location.cs
@@ -84,194 +84,146 @@ namespace location
                 Console.WriteLine("The request was in an invalid format!");
                 Environment.Exit(0);
             }
+            try 
+            {
+                if (!File.Exists(logfilelocation)) 
+                {
+                    File.Create(logfilelocation);
+                }
+                else if (!File.Exists(serverdatabaselocation)) 
+                {
+                    File.Create(serverdatabaselocation);
+                }
+            }
+            catch (Exception) 
+            {
+                Console.WriteLine("The selected path for the source or log file was invalid or inaccessable");
+                Environment.Exit(-69);
+            }
             query = query.Substring(0, query.Length - 1);
             try
             {
-                personID = query.Split(" ")[0];
-                locationID = query.Split(" ")[1];
+                string[] temp = query.Split(" ");
+                personID = temp[0];
+                locationID = string.Join(" ",temp[1..temp.Length]);
             }
             catch (Exception) { }
             try
             {
-                TcpClient client = new TcpClient();
-                client.ReceiveTimeout = timeout;
-                client.SendTimeout = timeout;
-                client.Connect(host, port);
-                StreamWriter sw = new StreamWriter(client.GetStream());
-                StreamReader sr = new StreamReader(client.GetStream());
-                sw.WriteLine(query);
-                sw.Flush();
-                string result = "";
-                while (sr.Peek()>=0) 
+                string requestFullFormat = "";//build requests
+                if (locationID == "")
                 {
-                    result += sr.ReadLine();
-                }
-                //if (File.Exists(logfilelocation))
-                //{
-                //    File.Delete(logfilelocation);
-                //}
-                if (request == requestType.whois)
-                {
-                    if (result != "OK\r\n")
+                    switch (request)
                     {
-                        Console.WriteLine(personID + " is " + result+"\r\n");
-                        File.AppendAllText(logfilelocation, personID + " is " + result+"\n");
-
-                    }
-                    else if (result == "OK\r\n")
-                    {
-                        Console.WriteLine(personID + " location changed to be " + locationID+"\r\n");
-                        File.AppendAllText(logfilelocation, personID + " location changed to be " + locationID+"\n");
+                        case requestType.whois:
+                            requestFullFormat = personID+"\r\n";
+                            break;
+                        case requestType.HTTP09:
+                            requestFullFormat = "GET /" + personID + "\r\n";
+                            break;
+                        case requestType.HTTP10:
+                            requestFullFormat = "GET /?" + personID + " HTTP/1.0\r\n\r\n";
+                            break;
+                        case requestType.HTTP11:
+                            requestFullFormat = "GET /?name=" + personID + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n";
+                            break;
                     }
                 }
-                else if (request == requestType.HTTP09 || request == requestType.HTTP10 || request == requestType.HTTP11)//HERE
+                else 
                 {
-                    Console.WriteLine(result+"\r\n");
-                    File.AppendAllText(logfilelocation, result+"\n");
+                    switch (request)
+                    {
+                        case requestType.whois:
+                            requestFullFormat = personID + " " + locationID + "\r\n";
+                            break;
+                        case requestType.HTTP09:
+                            requestFullFormat = "PUT /" + personID + "\r\n\r\n" + locationID + "\r\n";
+                            break;
+                        case requestType.HTTP10:
+                            requestFullFormat = "POST /" + personID + " HTTP/1.0\r\nContent-Length: " + locationID.Length + "\r\n\r\n" + locationID;
+                            break;
+                        case requestType.HTTP11:
+                            string body = "name=" + personID + "&location=" + locationID;
+                            requestFullFormat = "POST / HTTP/1.1\r\nHost: "+host+"\r\nContent-Length: "+body.Length+"\r\n\r\n"+body;
+                            break;
+                    }
                 }
+                Console.WriteLine(sendAndRespond(requestFullFormat,timeout,host,port,request,personID,locationID,logfilelocation));
             }
             catch (IOException) 
             {
                 Console.WriteLine("The Client Timed Out");
             }
         }
+        static string sendAndRespond(string requestFullFormat,int timeout, string host, int port,requestType request,string personID,string locationID,string logFileLocation) 
+        {
+            TcpClient client = new TcpClient();
+            client.ReceiveTimeout = timeout;
+            client.SendTimeout = timeout;
+            client.Connect(host, port);
+            StreamWriter sw = new StreamWriter(client.GetStream());
+            StreamReader sr = new StreamReader(client.GetStream());
+            sw.Write(requestFullFormat);//need to modify query for HTTP requests as server is only recieving them in whois format therefore not returning corrrectly
+            sw.Flush();
+            string response = "";
+            try
+            {
+                while (!sr.EndOfStream)
+                {
+                    response += (char)sr.Read();
+                }
+            }
+            catch (Exception)
+            {
+                if (response.Length == 0)
+                {
+                    Console.WriteLine("The request timed out!");
+                    Environment.Exit(-69);
+                }
+            }
+            string result = "";
+            if (request == requestType.whois)
+            {
+                if (locationID == "")
+                {
+                    result = personID + " is " + response + "\r\n";
+                    File.AppendAllText(logFileLocation, personID + " is " + response + "\n");
+
+                }
+                else
+                {
+                    result = personID + " location changed to be " + locationID + "\r\n";
+                    File.AppendAllText(logFileLocation, personID + " location changed to be " + locationID + "\n");
+                }
+            }
+            else if (request == requestType.HTTP09 || request==requestType.HTTP10 || request==requestType.HTTP11)
+            {
+                string[] lineresponse = response.Split("\r\n");
+                if (lineresponse[0] == "HTTP/0.9 200 OK" || lineresponse[0]=="HTTP/1.0 200 OK" || lineresponse[0]== "HTTP/1.1 200 OK")
+                {
+                    if (locationID == "")
+                    {
+                        result = personID + " is " + response.Split("\r\n\r\n")[1] + "\r\n";
+                    }
+                    else
+                    {
+                        result = personID + " location changed to be " + locationID + "\r\n";
+                        File.AppendAllText(logFileLocation, personID + " location changed to be " + locationID + "\n");
+                    }
+                }
+                else if (lineresponse[0] == "HTTP/0.9 200 OK" || lineresponse[0] == "HTTP/1.0 200 OK" || lineresponse[0] == "HTTP/1.1 200 OK")
+                {
+                    Console.WriteLine("Error: Not Found");
+                }
+                else 
+                {
+                    string trueresponse = response.Split("\r\n\r\n")[1];
+                    Console.WriteLine(personID+" is "+trueresponse.Substring(0,trueresponse.Length-2));
+                }
+                File.AppendAllText(logFileLocation, response + "\n");
+            }
+            return result;
+
+        }
     }
 }
-//        private static void whois(string[] args,TcpClient client)
-//        {
-//            if (args.Length != 0 && args.Length <= 2)
-//            {
-//                StreamWriter sw = new StreamWriter(client.GetStream());
-//                StreamReader sr = new StreamReader(client.GetStream());
-//                if (args.Length == 1)
-//                {
-//                    sw.WriteLine(args[0]);
-//                }
-//                else if (args.Length == 2)
-//                {
-//                    sw.WriteLine(args[0] + " " + args[1]);
-//                }
-//                sw.Flush();
-//                string result = sr.ReadToEnd();
-//                if (args.Length == 1)
-//                {
-//                    Console.WriteLine(args[0] + " is " + result);
-//                }
-//                else if (result == "OK\r\n" && args.Length == 2)
-//                {
-//                    Console.WriteLine(args[0] + " location changed to be " + args[1]);
-//                }
-//            }
-//            else if (args.Length == 0)
-//            {
-//                Console.WriteLine("No arguments where supplied!");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Too many arguments where supplied!");
-//            }
-//        }
-//        private static void HTTP09(string[] args,TcpClient client)
-//        {
-//            if (args.Length != 0 && args.Length <= 3)
-//            {
-//                StreamWriter sw = new StreamWriter(client.GetStream());
-//                StreamReader sr = new StreamReader(client.GetStream());
-//                if (args.Length == 2)
-//                {
-//                    sw.WriteLine(args[0] + " " + args[1]);
-//                }
-//                else if (args.Length == 3)
-//                {
-//                    sw.WriteLine(args[0] + " " + args[1] + " " + args[2]);
-//                }
-//                sw.Flush();
-//                string result = sr.ReadToEnd();
-//                if (args.Length == 2)
-//                {
-//                    Console.WriteLine(args[1] + " is " + result);
-//                }
-//                else if (result == "OK\r\n" && args.Length == 3)
-//                {
-//                    Console.WriteLine(args[1] + " location changed to be " + args[2]);
-//                }
-//            }
-//            else if (args.Length == 0)
-//            {
-//                Console.WriteLine("No arguments where supplied!");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Too many arguments where supplied!");
-//            }
-//        }
-//        private static void HTTP10(string[] args,TcpClient client)
-//        {
-//            if (args.Length != 0 && args.Length <= 3)
-//            {
-//                StreamWriter sw = new StreamWriter(client.GetStream());
-//                StreamReader sr = new StreamReader(client.GetStream());
-//                if (args.Length == 3)
-//                {
-//                    sw.WriteLine(args[0] + " " + args[1] + " " + args[2]);
-//                }
-//                else if (args.Length == 4)
-//                {
-//                    sw.WriteLine(args[0] + " " + args[1] + " " + args[2] + " " + args[3]);
-//                }
-//                sw.Flush();
-//                string result = sr.ReadToEnd();
-//                if (args.Length == 3)
-//                {
-//                    Console.WriteLine(args[1] + " is " + result);
-//                }
-//                else if (result == "OK\r\n" && args.Length == 4)
-//                {
-//                    Console.WriteLine(args[1] + " location changed to be " + args[3]);
-//                }
-//            }
-//            else if (args.Length == 0)
-//            {
-//                Console.WriteLine("No arguments where supplied!");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Too many arguments where supplied!");
-//            }
-//        }
-//        private static void HTTP11(string[] args,TcpClient client)
-//        {
-//            if (args.Length != 0 && args.Length <= 5)
-//            {
-//                StreamWriter sw = new StreamWriter(client.GetStream());
-//                StreamReader sr = new StreamReader(client.GetStream());
-//                if (args.Length == 4)
-//                {
-//                    sw.WriteLine(args[0] + " " + args[1] + " " + args[2]+" "+args[3]);
-//                }
-//                else if (args.Length == 5)
-//                {
-//                    sw.WriteLine(args[0] + " " + args[1] + " " + args[2] + " " + args[3]+" "+args[4]);
-//                }
-//                sw.Flush();
-//                string result = sr.ReadToEnd();
-//                if (args.Length == 3)
-//                {
-//                    Console.WriteLine(args[1] + " is " + result);
-//                }
-//                else if (result == "OK\r\n" && args.Length == 4)
-//                {
-//                    Console.WriteLine(args[1] + " location changed to be " + args[3]);
-//                }
-//            }
-//            else if (args.Length == 0)
-//            {
-//                Console.WriteLine("No arguments where supplied!");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Too many arguments where supplied!");
-//            }
-//        }
-//    }
-//}
